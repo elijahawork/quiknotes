86d7124af31cc8714dedd9dec854ee2f675811e2
Eli Simkin <es340716@g.risd.org>
2021-02-15 23:24:49 -0600

    Simplified index and added a basic test for whether the project exists

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,13 @@
-import { join } from "path";
-import { ClassContentController } from "./controllers/ClassContentController";
-import { ClassController } from "./controllers/ClassController";
-import { ClassContentModel } from "./models/ClassContentModel";
-import { ClassModel } from "./models/ClassModel";
-import { AssignmentView } from "./view/AssignmentView";
-import { ClassEditorView } from "./view/ClassEditorView";
-import { SourceView } from "./view/SourceView";
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
 
 export const __PROJ_NAME = join(__dirname, 'protected');
 
 function test() {
-    const sview = new SourceView('Pearson');
-    
-    sview.attachToHTMLElement(document
-        .getElementsByClassName('assignment-sources')[0] as HTMLElement);
-        
-    console.log(sview);
-    
-    const assignment1 = new AssignmentView('Unit I Essay', 'DUE SOON', undefined, 'soon');
-    const assignment2 = new AssignmentView('Unit II Essay', 'DUE IN 6 DAYS', undefined, 'far');
-    
-    sview.addAssignment(assignment1);
-    sview.addAssignment(assignment2);
-
-    const sview2 = new SourceView('Google Classroom');
-
-    sview2.attachToHTMLElement(document
-        .getElementsByClassName('assignment-sources')[0] as HTMLElement);
-    
-    const assignment3 = new AssignmentView('Unit I Essay', 'DUE SOON', undefined, 'soon');
-    const assignment4 = new AssignmentView('Unit II Essay', 'DUE IN 6 DAYS', undefined, 'far');
-    sview2.addAssignment(assignment3);
-    sview2.addAssignment(assignment4);
-
-    const classModel = ClassContentModel.fromPath(0);
-    const classView = new ClassEditorView('notes');
-    const controller = new ClassContentController(classModel, classView);
-    classView.attachToHTMLElement(document.getElementById('class-notes')!);
+  if (!existsSync(__PROJ_NAME)) mkdirSync(__PROJ_NAME);
 }
-function init() {
-
-}
+function init() {}
 export function main() {
-    test();
-    init();
+  test();
+  init();
 }
