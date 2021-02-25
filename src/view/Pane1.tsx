1b310a10c74808865be0924c8ab107ccc7ba1dac
Eli Simkin <es340716@g.risd.org>
2021-02-25 11:11:59 -0600

    Got the first pane class addition working

--- src/view/Pane1.tsx
+++ src/view/Pane1.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { CLASS_LIST } from '..';
+import React, { createRef } from 'react';
 import { ClassModel } from '../models/ClassModel';
-import { AppState, AppProps } from './App';
+import { AppProps, AppState } from './App';
 
 type Pane1Props = {
   classes: ClassModel[];
-  setState: <K extends keyof AppState>(state: AppState | ((prevState: Readonly<AppState>, props: Readonly<AppProps>) => AppState | Pick<AppState, K> | null) | Pick<AppState, K> | null, callback?: (() => void) | undefined) => void
+  setState: <K extends keyof AppState>(
+    state:
+      | AppState
+      | ((
+          prevState: Readonly<AppState>,
+          props: Readonly<AppProps>
+        ) => AppState | Pick<AppState, K> | null)
+      | Pick<AppState, K>
+      | null,
+    callback?: (() => void) | undefined
+  ) => void;
 };
-type Pane1State = {} & Pane1Props;
+type Pane1State = { classes: ClassModel[] };
 
 class Pane1 extends React.Component<Pane1Props, Pane1State> {
   constructor(props: Pane1Props) {
@@ -16,26 +25,37 @@ class Pane1 extends React.Component<Pane1Props, Pane1State> {
   }
 
   render() {
+    const inp = createRef<HTMLInputElement>();
+
     return (
       <div className={'pane'}>
         <header>
+          <input type="text" ref={inp} />
           <button
             onClick={() => {
-              const className = prompt('Name this new class.');
-
-              if (className) CLASS_LIST.push(new ClassModel(className, [], ''));
+              if (!inp.current?.value) alert('no');
+              this.setState((prevState) => ({
+                classes: [
+                  ...prevState.classes,
+                  new ClassModel(inp.current!.value, [], ''),
+                ]
+              }));
             }}
           >
             +
           </button>
         </header>
         <ul>
-          {this.state.classes.map((cModel) => {
+          {this.state.classes.map((cModel, key) => {
             return (
-              <li>
-                <button onClick={() => {
-                  this.props.setState({ modelOpen: cModel });
-                }}>{cModel.name}</button>
+              <li key = { key }>
+                <button key = { key + 1 }
+                  onClick={() => {
+                    this.props.setState({ modelOpen: cModel });
+                  }}
+                >
+                  {cModel.name}
+                </button>
               </li>
             );
           })}
