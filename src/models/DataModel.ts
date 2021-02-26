4065a2e0dd0c78b62ce4bd7bffdc05a9ec8aa966
Eli Simkin <es340716@g.risd.org>
2021-02-26 12:48:24 -0600

    Renamed fileMap to dataModelRegistry

--- src/models/DataModel.ts
+++ src/models/DataModel.ts
@@ -1,12 +1,12 @@
 import Writable from '../interfaces/Writable';
 import ISchema from '../schema/ISchema';
 
-const fileMap = new Map<number, DataModel<any>>();
+const dataModelRegistry = new Map<number, DataModel<any>>();
 export abstract class DataModel<Schema> implements Writable {
   protected abstract schema: Schema;
 
   constructor(id: number) {
-    fileMap.set(id, this);
+    dataModelRegistry.set(id, this);
   }
   abstract updateFile(): void;
 
@@ -14,14 +14,14 @@ export abstract class DataModel<Schema> implements Writable {
     if (id === undefined)
       // creates a unique ID. May hypothetically run into a bug if the math doesn't work out but IDK yet
       while (
-        fileMap.has((id = Math.floor(Math.random() * (fileMap.size + 1) * 2)))
+        dataModelRegistry.has((id = Math.floor(Math.random() * (dataModelRegistry.size + 1) * 2)))
       );
 
     return id;
   }
 
   static getById(id: number): DataModel<ISchema> | null {
-    if (fileMap.has(id)) return fileMap.get(id)!;
+    if (dataModelRegistry.has(id)) return dataModelRegistry.get(id)!;
     return null;
   }
   public stringify(): string {
