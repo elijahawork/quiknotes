2d905c93165f8bd64fd273dfd709e11154713331
Eli Simkin <es340716@g.risd.org>
2021-02-28 23:37:04 -0600

    Improved file population

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { readdirSync } from 'fs';
 import { join, parse } from 'path';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AssignmentModel } from './models/AssignmentModel';
 import { ClassModel } from './models/ClassModel';
 import App from './view/App';
 export const __PROJ_NAME = join(__dirname, '..', 'protected');
@@ -10,7 +11,7 @@ export const CLASS_LIST: ClassModel[] = [];
 
 const root = document.getElementById('root')!;
 
-function parseClassList() {
+function populateListings() {
   const dir = readdirSync(__PROJ_NAME);
   for (const file of dir) {
     const { ext, name } = parse(file);
@@ -22,11 +23,16 @@ function parseClassList() {
           CLASS_LIST.push(classModel);
         }
         break;
+      case AssignmentModel.EXT:
+        {
+          // create the assignment in the registry
+          AssignmentModel.fromPath(parseInt(name));
+        }
     }
   }
 }
 
-export const app = (parseClassList(), (<App classes={CLASS_LIST} />));
+export const app = (populateListings(), (<App classes={CLASS_LIST} />));
 
 function test() {}
 function init() {
